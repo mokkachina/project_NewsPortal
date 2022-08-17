cb3b285765ed0234267243b8714adf9fd0eaf267
Maksim Klimov <maxiklim80@gmail.com>
2022-08-17 18:07:28 +0600

    0001_initiall_create new db

--- NewsPaper/accounts/models.py
+++ NewsPaper/accounts/models.py
@@ -45,6 +45,9 @@ class Post(models.Model):
     def previwe(self):
         return self.text[0:123] + "..."
 
+    def __str__(self):
+        return f'{self.author.authorUser.username}'
+
 
 class PostCategory(models.Model):
     postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
